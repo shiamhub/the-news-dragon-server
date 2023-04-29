e2e31845ba371cea38d97d0f7323fb4c2298f127
shiamhub <skshiam@gmail.com>
2023-04-29 13:24:48 +0600

    vercel config

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categorise.json');
+const news = require('./data/news.json');
+const e = require('express');
 
 app.use(cors())
 
@@ -13,6 +15,29 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    if(parseInt(id) === 0) {
+        res.send(news);
+    }
+    else{
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
 })
 
 app.listen(port, () => {
